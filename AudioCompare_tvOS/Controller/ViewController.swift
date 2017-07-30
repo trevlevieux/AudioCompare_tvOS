806e05fb78a46d78090d68641034cda02eb3d723
Trevor LeVieux <Trevor@BuildingBlocksMedia.com>
2017-07-29 18:58:54 -0700

    tvOS 11 updates

---
+++ AudioCompare_tvOS/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioCompare_tvOS
+//
+//  Created by Trevor LeVieux on 7/24/17.
+//  Copyright © 2017 LeVieux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+
+# ignore all warnings from all pods
+inhibit_all_warnings!
+use_frameworks!
+
+source 'https://github.com/CocoaPods/Specs.git'
+platform :tvos, '9.0'
+project 'AudioCompare_tvOS.xcodeproj'
+
+target 'AudioCompare_tvOS' do
+
+	#pod 'AudioKit'
+
+end
