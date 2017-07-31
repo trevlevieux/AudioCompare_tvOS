b3947de99040780b0357bb2247d1cfe50255ea20
Trevor LeVieux <Trevor@BuildingBlocksMedia.com>
2017-07-31 15:56:49 -0700

    UI updates

--- AudioCompare_tvOS/Controller/Main_ContainerViewController.swift
+++ AudioCompare_tvOS/Controller/Main_ContainerViewController.swift
@@ -1,14 +1,14 @@
 //
-//  ViewController.swift
+//  Main_ContainerViewController.swift
 //  AudioCompare_tvOS
 //
-//  Created by Trevor LeVieux on 7/24/17.
+//  Created by Trevor LeVieux on 7/23/17.
 //  Copyright © 2017 LeVieux. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class Main_ContainerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
 }
 
